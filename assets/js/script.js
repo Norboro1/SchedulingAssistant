6b707be671557ef6586e3817da4c47a0cdb9a402
Norboro <ajbrook19@gmail.com>
2023-02-03 13:51:29 -0500

    Grab current date and format for display.
    Add days advanced formatting for ordinal on day.
    
    Build function to load/render schedule with for loop.
    
    Create Set Interval function to update the current hour every 30 seconds.
    
    Add functionality to save button to properly save user input to localStorage
    
    Added functionality to interval function to update schedule colors if the hour changes while user is using site (Only works logically, if hour advances by just one).

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,90 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+var currentDayEl = $('#currentDay');
+var scheduleEl = $('#scheduleDisplay');
+var today = dayjs();
+var currentHour = parseInt(today.format('H'));
+var tasks = JSON.parse(localStorage.getItem('tasks'));
+
+if(!tasks){
+  tasks = {};
+  for (i=9; i<18; i++){
+    tasks[i]='';
+  }
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+setInterval(function(){
+  today = dayjs();
+  console.log('hour variable: '+currentHour + '\nactual hour: ' +parseInt(today.format('H')));
+  if(parseInt(today.format('H')) > currentHour){
+    currentHour = parseInt(today.format('H'));
+    reloadColors();
+  }
+  currentHour = parseInt(today.format('H'));
+}, 30000);
+
+currentDayEl.text(today.format('dddd, MMMM Do'));
+
+function loadSchedule(){
+  var hour = 9;
+  var ampm;
+  for (i=0; i<9; i++){
+    if(hour > 12){
+      var hourProper = hour-12;
+      ampm = "PM";
+    } else if (hour ==12){
+      hourProper = hour;
+      ampm = "PM";
+    } else {
+      hourProper = hour;
+      ampm = "AM";
+    }
+
+    var timeBlock = $('<div id='+hour+' class="row time-block">');
+    var blockTitle = $('<div class="col-2 col-md-1 hour text-center py-3">');
+    var blockTextArea = $('<textarea class="col-8 col-md-10 description" rows="3">');
+    var blockButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save">');
+    blockButton.append('<i class="fas fa-save" aria-hidden="true"></i>');
+
+    blockTextArea.text(tasks[hour]);
+
+    if(hour < currentHour) {
+      timeBlock.addClass('past');
+    } else if (hour == currentHour){
+      timeBlock.addClass('present');
+    } else {
+      timeBlock.addClass('future');
+    }
+
+    blockTitle.text(hourProper.toString()+ampm);
+    timeBlock.append(blockTitle, blockTextArea, blockButton);
+    scheduleEl.append(timeBlock);
+
+    hour++;
+  }
+
+}
+
+function reloadColors(){
+  for(i=9; i<18; i++){
+    if(i < currentHour) {
+      $("#"+i).removeClass('present');
+      $("#"+i).removeClass('past');
+      $("#"+i).addClass('past');
+    } else if (i == currentHour){
+      $("#"+i).removeClass('future');
+      $("#"+i).removeClass('present');
+      $("#"+i).addClass('present');
+    } 
+  }
+}
+
+scheduleEl.on('click', 'button', function(){
+  var currentID = $(this).parent().attr('id');
+  tasks[currentID] = $(this).siblings('textarea').val();
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+})
+
+loadSchedule();
